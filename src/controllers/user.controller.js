c409e3b08a4564202076aa18bd92a47d4f884a8e
Samarth <samarthpawarsamarthpawar@gmail.com>
2026-02-02 23:53:35 +0530

    p4

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,23 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async(req,res) => {
-    res.status(200).json({
-        message:"Samarth Well Done ! User Registered Successfully"
-    })
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exists: username , email
+    // check for images, check for avatar
+    // upload them to cloudinary , avatar
+    // create user object --create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return res
+
+
+    const {fullName, email , username , password } =  req.body
+
+    console.log("email:", email);
+    console.log("pass:", password);
+     
 } )
 
 
-export { registerUser }
+export { registerUser } 
