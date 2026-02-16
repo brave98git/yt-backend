b7a1247dc2d49e665a74b0c2e85aeafa43bf639f
Samarth <samarthpawarsamarthpawar@gmail.com>
2026-02-16 19:06:50 +0530

    feat: implement like and subscription controllers with routes
    - Add like router to app.js
    - Implement toggle subscription, get subscribers, and get subscribed channels
    - Implement toggle like for video, comment, and tweet with get liked videos
    - Add validation for IDs and proper response handling

--- src/app.js
+++ src/app.js
@@ -28,6 +28,9 @@ import videoRouter from './routes/video.routes.js';
 import tweetRouter from './routes/tweet.routes.js';
 import commentRouter from './routes/comment.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
+import likeRouter from './routes/like.routes.js';
+
+
 
 //routes declaration
 
@@ -36,6 +39,7 @@ app.use("/api/v1/videos",videoRouter);
 app.use("/api/v1/tweets",tweetRouter);
 app.use("/api/v1/comments",commentRouter);
 app.use("/api/v1/subscriptions",subscriptionRouter);
+app.use("/api/v1/likes",likeRouter);
 
 //eg.http://localhost:8000/api/v1/users/register
 
--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -3,26 +3,85 @@ import {Like} from "../models/like.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { Tweet } from "../models/tweet.model.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     //TODO: toggle like on video
+    if(!videoId || !isValidObjectId(videoId)){
+        throw new ApiError(400,"VideoId is required")
+    }
+
+    const likedVideo = await Like.findOne({
+        video: videoId,
+        likedby: req.user._id
+    })
+    if(likedVideo){
+        await Like.findByIdAndDelete(likedVideo._id)
+        return res.status(200).json(new ApiResponse(200,{}, "Video unliked"))
+    }
+    await Like.create({
+        video: videoId,
+        likedby: req.user._id
+    })
+    return res.status(200).json(new ApiResponse(200,{}, "Video liked"))
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
-
+    if(!commentId || !isValidObjectId(commentId)){
+        throw new ApiError(400,"CommentId is required")
+    }
+    const likedComment = await Like.findOne({
+        comment: commentId,
+        likedby: req.user._id
+    })
+    if(likedComment){
+        await Like.findByIdAndDelete(likedVideo._id)
+        return res.status(200).json(new ApiResponse(200,{}, "Comment unliked"))
+    }
+    await Like.create({
+        comment: commentId,
+        likedby: req.user._id
+    })
+    return res.status(200).json(new ApiResponse(200,{}, "Comment liked"))
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
+    if(!tweetId || !isValidObjectId(tweetId)){
+        throw new ApiError("Tweet Is Required");
+    }
+
+    const likedTweet = await Tweet.findOne({
+        tweet: tweetId,
+        likedby: req.user._id
+    })
+
+    if(likedTweet){
+        await Like.findByIdAndDelete(likedTweet._id)
+        return res.status(200).json(new ApiResponse(200,{}, "Tweet unliked"))
+    }
+    await Like.create({
+        tweet: tweetId,
+        likedby: req.user._id
+    })
+    return res.status(200).json(new ApiResponse(200,{}, "Tweet liked"))
+
 }
 )
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+    const likedVideos = await Like.find({
+        likedby: req.user._id
+    }).populate({
+        path: "video",
+        model: "Video"
+    })
+    return res.status(200).json(new ApiResponse(200,likedVideos, "Liked Videos"))
 })
 
 export {
